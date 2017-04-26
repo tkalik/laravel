941afaf3802b6da0cadd2c0fa8b56e0dda4aaec7
tkalik <tomislavkalik@gmail.com>
2017-04-26 20:50:24 +0200

    Dodan view za show itema

---
+++ resources/views/items/show.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+
+    <head>
+        <title>Laravel projekt</title>
+    </head>
+    <body>
+        <h2>{{ $item->title }}</h2>
+        <div>{{ $item->content }}</div>
+        
+    </body>
+</html>
